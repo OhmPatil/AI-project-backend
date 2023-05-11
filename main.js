1410b81153fc01a3f18ab9a6669c39e471306274
OhmPatil <workpatil10@gmail.com>
2023-05-12 04:41:03 +0530

    Remove main.js from .gitignore

---
+++ main.js
@@ -0,0 +1,17 @@
+const fetch = require("node-fetch");
+
+/////////////////////////////
+async function fetchResults(subreddit) {
+  let results = await fetch(`https://www.reddit.com/r/${subreddit}/hot/.json`);
+  let data = await results.json();
+
+  let headlines = [];
+  data.data.children.forEach((post) => {
+    headlines.push(post.data.title);
+  });
+
+  // console.log(headlines);
+  return headlines;
+}
+
+module.exports = {fetchResults}
