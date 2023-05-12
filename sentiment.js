95ea159017091598491d951140d6d95db10eeef5
OhmPatil <workpatil10@gmail.com>
2023-05-12 10:04:15 +0530

    Add comments and cleanup code

--- sentiment.js
+++ sentiment.js
@@ -2,20 +2,23 @@ const { SentimentAnalyzer, PorterStemmer, WordTokenizer } = require("natural");
 const { removeStopwords } = require("stopword");
 
 function getSentiment(text) {
+  // Remove special symbols etc
   const cleanedText = text.replace(/[^a-zA-Z\s]+/g, "");
 
+  // Tokenize text
   const tokenizer = new WordTokenizer();
   const tokenizedText = tokenizer.tokenize(cleanedText);
 
-  const filteredText = removeStopwords(tokenizedText);
+  // Remove stopwords
+  const filteredText = removeStopwords(tokenizedText); //Returns array of words(tokens)
 
+  // Analyze
   const analyzer = new SentimentAnalyzer("English", PorterStemmer, "afinn");
   let result = analyzer.getSentiment(filteredText);
   result = result.toFixed(2);
 
   // console.log(result);
   return parseFloat(result);
-  // return cleanedText
 }
 
 module.exports = { getSentiment };
